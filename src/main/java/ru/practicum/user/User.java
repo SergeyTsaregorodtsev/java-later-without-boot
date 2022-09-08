840b2b7e308b4581ad84b9859345e16d7d7d4795
Se.Tsaregorodtsev <Se.Tsaregorodtsev@yandex.ru>
2022-09-08 22:04:58 +0300

    #3 JPA repository added

--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -1,12 +1,34 @@
 package ru.practicum.user;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 @Data
-@AllArgsConstructor
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
+    @Id
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
     private String email;
-    private String name;
+
+    @Column(name = "registration_date")
+    private LocalDateTime registrationDate = LocalDateTime.now();
+
+    @Column(name = "state")
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+
+    enum UserState {
+        ACTIVE, BLOCKED, DELETED;
+    }
 }
